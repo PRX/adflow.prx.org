d2b285e75718782024e1099fc97a3d9c609f1d48
gcampo88 <gigi.campo@gmail.com>
2017-10-27 10:38:21 -0400

    skeleton for app component testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,74 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Subject } from 'rxjs/Subject';
+import { CoreModule} from './core'; //, CmsService
+
+import { AuthModule, AuthService, ModalModule } from 'ngx-prx-styleguide'; //, MockHalService, MockHalDoc
+
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
+  let comp: AppComponent;
+  let fix: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let auth;
+  // let cms;
+  let authToken;
+  let refreshToken;
+  // let cmsToken: string = null;
+
   beforeEach(async(() => {
+    authToken = new Subject<string>();
+    refreshToken = new Subject<boolean>();
+
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      imports: [
+        CoreModule,
+        AuthModule,
+        ModalModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: AuthService, useValue: {
+          config: () => {},
+          url: () => '',
+          token: authToken,
+          refresh: refreshToken
+        }}
+      ]
+    }).compileComponents().then(() => {
+      fix = TestBed.createComponent(AppComponent);
+      comp = fix.componentInstance;
+      fix.detectChanges();
+      de = fix.debugElement;
+      el = de.nativeElement;
+    });
   }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(comp).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Adflow');
+  //
+  // it(`should only show podcast choices when logged in`, async(() => {
+  //   comp.loggedIn = true;
+  //   fix.detectChanges();
+  //   expect(de.query(By.css('adflow-podcasts'))).not.toBeNull();
+  //   comp.loggedIn = false;
+  //   fix.detectChanges();
+  //   expect(de.query(By.css('adflow-podcasts'))).toBeNull();
+  // }));
+
+  it('should show user info when logged in', async(() => {
+    comp.loggedIn = true;
+    fix.detectChanges();
+    expect(de.query(By.css('prx-navuser'))).toBeTruthy();
+    comp.loggedIn = false;
+    fix.detectChanges();
+    expect(de.query(By.css('prx-navuser'))).toBeNull();
   }));
 });
