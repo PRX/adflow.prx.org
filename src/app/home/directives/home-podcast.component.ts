1228d49ca4fcd9c6154a7d721b57f885554ba2f9
gcampo88 <gigi.campo@gmail.com>
2017-11-08 12:13:55 -0500

    first crack at Podcast Model

--- src/app/home/directives/home-podcast.component.ts
+++ src/app/home/directives/home-podcast.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HalDoc } from '../../core';
+import { PodcastModel } from '../../shared'; // CampaignModel too
 
 @Component({
   selector: 'adflow-home-podcast',
@@ -8,27 +9,30 @@ import { HalDoc } from '../../core';
   <header>
     <h1><a href="#">{{podcast.name}}</a></h1>
   </header>
-  <div class="campaign-list" *ngIf="campaigns && campaigns.length">
-    <adflow-home-campaign *ngFor="let c of campaigns" [campaign]="c"></adflow-home-campaign>
+  <div class="campaign-list" *ngIf="podcast.campaigns && podcast.campaigns.length">
+    <adflow-home-campaign *ngFor="let c of podcast.campaigns" [campaign]="c"></adflow-home-campaign>
   </div>
   `
 })
 
-export class HomePodcastComponent implements OnInit {
+export class HomePodcastComponent { //implements OnInit {
 
-  @Input() podcast: HalDoc;
+  @Input() podcast: PodcastModel;
 
-  campaigns: HalDoc[]; // TODO should change to Campaign Model
+  // we should be able to get this all from podcast.campaigns once PodcastModel related() is working
+  //campaigns: HalDoc[]; // TODO should change to Campaign Model
 
-  ngOnInit() {
-    this.loadCampaigns();
-  }
+  // ngOnInit() {
+  //   console.log(' in home podcast oninit')
+  //   // this.loadCampaigns();
+  // }
 
-  loadCampaigns() {
-    if (this.podcast && this.podcast.has('prx:campaigns')) {
-      this.podcast.followItems('prx:campaigns').subscribe(campaigns => {
-        this.campaigns = campaigns;
-      });
-    }
-  }
+  // loadCampaigns() {
+  //   // follow doc
+  //   if (this.podcast.doc && this.podcast.doc.has('prx:campaigns')) {
+  //     this.podcast.doc.followItems('prx:campaigns').subscribe(campaigns => {
+  //       this.campaigns = campaigns;
+  //     });
+  //   }
+  // }
 }
---
+++ src/app/shared/model/podcast.model.ts
@@ -0,0 +1,68 @@
+import { BaseModel, HalDoc } from 'ngx-prx-styleguide';
+import { Observable } from 'rxjs/Observable';
+
+export class PodcastModel extends BaseModel {
+  public id: number;
+  public doc: HalDoc;
+  public name: string;
+  public network: string;
+  public notes: string;
+  public rate: string;
+  public structure: string;
+  public recordingDay: string;
+  public campaigns = []; //CampaignModel[] = [];
+
+  SETABLE = []; // podcasts are read only
+
+  VALIDATORS = {};
+
+  constructor(podcast: HalDoc, loadRelated = true) {
+    super();
+    this.init(null, podcast, loadRelated);
+  }
+
+  key(): string {
+    if (this.doc) {
+      return `prx.podcast.${this.doc.id}`;
+    }
+  }
+
+  related(): {} {
+    let campaigns = Observable.of([]);
+    if (this.doc && this.doc.has('prx:campaigns')) {
+      console.log('has campaigns')
+      campaigns = this.doc.followItems('prx:campaigns'); // turn to CampaignModel
+    }
+    return { campaigns: campaigns };
+  }
+
+  decode(): void {
+    this.id = this.doc['id'];
+    this.name = this.doc['name'];
+    this.network = this.doc['network'];
+    this.notes = this.doc['notes'];
+    this.rate = this.doc['rate'];
+    this.structure = this.doc['structure'];
+    this.recordingDay = this.doc['recordingDay'];
+  }
+
+  encode(): {} {
+    let data = <any> {};
+    data.id = this.id;
+    data.name = this.name;
+    data.network = this.network;
+    data.notes = this.notes;
+    data.rate = this.rate;
+    data.structure = this.structure;
+    data.recordingDay = this.recordingDay;
+    // data.campaigns = this.campaigns;
+
+    // data.field = this.field;
+    // returns an object with the model's data used in persisting the underlying HalDoc
+    return data;
+  }
+
+  saveNew(data: {}): Observable<HalDoc> {
+    return Observable.throw(new Error('Cannot directly create a jingle podcast'));
+  }
+}
