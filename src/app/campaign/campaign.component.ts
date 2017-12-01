e30bb6808d53f31aa3dd2b1f0c43e3820a3e87ba
gcampo88 <gigi.campo@gmail.com>
2017-12-01 15:54:57 -0500

    styleguide is the best

--- src/app/campaign/campaign.component.ts
+++ src/app/campaign/campaign.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
-// import { CmsService } from '../core';
+import { JingleService } from '../core';
 import { TabService  } from 'ngx-prx-styleguide'; //ModalService, ToastrService
 import { CampaignModel } from '../shared';
 
@@ -17,50 +17,53 @@ import { CampaignModel } from '../shared';
 export class CampaignComponent implements OnInit {
 
   id: number;
+  base: string;
   campaign: CampaignModel;
+  isLoaded = false;
 
   constructor(
     // private modal: ModalService,
     // private toastr: ToastrService,
+    private jingle: JingleService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
 
   ngOnInit() {
+    this.isLoaded = false;
     this.route.params.forEach(params => {
      this.id = +params['id'];
+     this.base = '/campaign/' + this.id;
      this.loadCampaign();
    });
   }
 
   loadCampaign() {
     if (this.id) {
-      // this.cms.auth.follow('prx:campaign', {id: this.id}).subscribe(
-      //   c => {
-      //     //  set campaign
-      //   },
-      //   err => {
-      //     if (err.status === 404 && err.name === 'HalHttpError') {
-      //       this.toastr.error('No campaign found. Redirecting to home');
-      //       console.error(`Campaign with id ${this.id} not found`);
-      //       setTimeout(() => this.router.navigate(['/']), 3000);
-      //     } else {
-      //       throw(err);
-      //     }
-      //   }
-      // );
+      this.jingle.follow('prx:campaign', {id: this.id}).subscribe(
+        (cDoc) => {
+          this.isLoaded = true;
+          this.setCampaign(null, cDoc)
+        },
+        err => {
+          if (err.status === 404 && err.name === 'HalHttpError') {
+            // this.toastr.error('No campaign found. Redirecting to home');
+            console.error(`Campaign with id ${this.id} not found`);
+            setTimeout(() => this.router.navigate(['/']), 3000);
+          } else {
+            throw(err);
+          }
+        }
+      )
     }
   }
 
   setCampaign(parent: any, campaign: any) {
-    // this.campaign = new CampaignModel(parent, campaign);
-
+    this.campaign = new CampaignModel(parent, campaign);
   }
 
   save() {
-    this.campaign.save().subscribe(() => {
-      // this.toastr.success('Campaign saved');
-    });
+    this.campaign.save();
   }
 
   discard() {
@@ -71,26 +74,26 @@ export class CampaignComponent implements OnInit {
     // not sure we should allow them to delete whole campaign?
   }
 
-  canDeactivate(next: any, prev: any): boolean | Observable<boolean> {
-    if (this.campaign && this.campaign.changed() && !this.campaign.isDestroy) {
-      let thatsOkay = new Subject<boolean>();
-      // this.modal.confirm(
-      //   'Unsaved changes',
-      //   `This campaign has unsaved changes. You may discard the changes and
-      //     continue or click 'Cancel' to complete and save the campaign.`,
-      //   (confirm: boolean) => {
-      //     if (confirm) {
-      //       this.discard();
-      //     }
-      //     thatsOkay.next(confirm);
-      //     thatsOkay.complete();
-      //   },
-      //   'Discard'
-      // );
-      return thatsOkay;
-    } else {
-      return true;
-    }
-  }
+  // canDeactivate(next: any, prev: any): boolean | Observable<boolean> {
+  //   if (this.campaign && this.campaign.changed() && !this.campaign.isDestroy) {
+  //     let thatsOkay = new Subject<boolean>();
+  //     // this.modal.confirm(
+  //     //   'Unsaved changes',
+  //     //   `This campaign has unsaved changes. You may discard the changes and
+  //     //     continue or click 'Cancel' to complete and save the campaign.`,
+  //     //   (confirm: boolean) => {
+  //     //     if (confirm) {
+  //     //       this.discard();
+  //     //     }
+  //     //     thatsOkay.next(confirm);
+  //     //     thatsOkay.complete();
+  //     //   },
+  //     //   'Discard'
+  //     // );
+  //     return thatsOkay;
+  //   } else {
+  //     return true;
+  //   }
+  // }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthGuard, DeactivateGuard, UnauthGuard, ImageModule, SpinnerModule } from 'ngx-prx-styleguide';
+import { AuthGuard, DeactivateGuard, UnauthGuard, FancyFormModule, HeroModule, ImageModule, ModalModule, SpinnerModule } from 'ngx-prx-styleguide';
 
 @NgModule({
   declarations: [],
   exports: [
     CommonModule,
+    FancyFormModule,
     ImageModule,
+    HeroModule,
+    ModalModule,
     SpinnerModule
   ],
   imports: [
     CommonModule,
+    FancyFormModule,
     ImageModule,
+    HeroModule,
+    ModalModule,
     SpinnerModule
   ],
   providers: [
